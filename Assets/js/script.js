5b544ec84a860f8b69e28c0834053660aca5e365
amaher <amaher112@su.edu>
2023-12-21 13:53:48 -0500

    added HTML divs for css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,6 @@ var cardGroup = document.querySelector('.card-group');
 
 function getWeather() {
   var cityInput = city.value;
-  console.log(cityInput);
 
    // Function to get coordinates
    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + APIKey)
@@ -12,43 +11,52 @@ function getWeather() {
    .then((data) => {
      var lat = data[0].lat;
      var lon = data[0].lon;
-     console.log({lat, lon});
+
+    // Function to get weather information from coordinates
      fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial")
      .then((response) => response.json())
      .then(weatherData => {
        console.log(weatherData);
-       console.log(weatherData.list[0].dt_txt)
-       
-
+    
+      //  Finds the first forecast at 12 noon
        var firstIndex = weatherData.list.findIndex(findFirstForecast);
-       console.log(firstIndex)
-       for (var i= firstIndex; i < weatherData.list.length; i++) {
-        console.log(i)
-        console.log(weatherData.list.length)
+
+      //  Loops through each 3-hour increment to find the 12 noon of each day
+       for (var i= firstIndex; i < weatherData.list.length; i+=8) {
+      
         var weatherDiv = document.createElement('div')
         weatherDiv.setAttribute('class', 'card')
         var weatherCardBody = document.createElement('div');
         weatherCardBody.setAttribute('class', 'card-body bg-info-subtle')
+        
+        // Creating elements for weather data
         var weatherDate = document.createElement('h5')
         weatherDate.setAttribute('class', 'card-title')
         var icon = document.createElement('img')
         icon.setAttribute('src', `https://openweathermap.org/img/wn/${weatherData.list[i].weather[0].icon}.png`)
         weatherDate.textContent = weatherData.list[i].dt_txt.split(' ')[0]
-        weatherCardBody.append(weatherDate)
-        weatherCardBody.append(icon)
-
+        var temp = document.createElement('h6');
+        temp.textContent = "Temp: " + weatherData.list[i].main.temp + " °F"
+        var wind = document.createElement('h6');
+        wind.textContent = "Wind: " + weatherData.list[i].wind.speed + " MPH"
+        var humidity = document.createElement('h6')
+        humidity.textContent = "Humidity: " + weatherData.list[i].main.humidity + " %"
+   
+       
+      //  Append weather data
+        weatherCardBody.append(weatherDate, icon, temp, wind, humidity)
         weatherDiv.append(weatherCardBody)
         cardGroup.append(weatherDiv)
-        console.log(cardGroup)
-        i=i+7
+      
+      
       }
        
       
-    //  Add more h5 tags for wind, temp , append on 39
+
     // First one in array is current weather
     // Rigth col will have two rows
     // left column has search
-       // Need to get temp, wind and humidity
+      
      })
      
    });
@@ -56,7 +64,6 @@ function getWeather() {
 }
 
 function findFirstForecast(singleWeatherData) {
-  console.log('insidestring')
   var hour = singleWeatherData.dt_txt.split(' ')
   return hour[1] == '12:00:00'
   
